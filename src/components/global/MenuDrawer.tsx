e4ba6eff80a1f288167f50562fd75ad8bc9f7db8
ThePaulin <kubwirapaulin@gmail.com>
2023-05-30 20:19:18 -0400

    fix animation

--- src/components/global/MenuDrawer.tsx
+++ src/components/global/MenuDrawer.tsx
@@ -18,16 +18,13 @@ function MenuDrawer ({ open, onClose }: { open: boolean, onClose: () => void }):
         <ul className="flex flex-col items-center gap-8 py-12 mt-12">
           {menuLinks.map((el) => {
             return (
-              <motion.li
-                initial={{ opacity: 0 }}
-                whileInView={{ opacity: 1 }}
-                key={el.title}
-                className=" w-fit transition-all duration-300 text-secondary/60 hover:text-secondary hover:font-700 hover:translate-x-1 "
-              >
+              <li
+              className=" w-fit transition-all duration-300 text-secondary/60 hover:text-secondary hover:font-700 hover:translate-x-1 "
+              key={el.title}>
                 <Link href={el.link} className="" onClick={onClose}>
                   {el.title}
                 </Link>
-              </motion.li>
+              </li>
             )
           })}
         </ul>
